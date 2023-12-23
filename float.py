cec28dc3a18942f16e10035eb40f3014bc154db1
Asbras <silentbob320@gmail.com>
2023-12-23 18:42:04 -0300

    Adicionando novos arquivos

---
+++ float.py
@@ -0,0 +1,8 @@
+# Float é uma abreviação de ponto flutuante
+# Esse é o mesmo programa que o inteiro.py, porem agora voce pode somar quaisquer numeros reais.
+x = float(input("Qual é o valor de X? "))
+y = float(input("Qual é o valor de Y? "))
+
+z = round(x + y)
+# Escrever na tela o resultado da soma de x e y. 
+print(f"{z:,}")
