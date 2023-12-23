bd591602b3fa6d5184b837bf3a3af0381944ea04
Asbras <silentbob320@gmail.com>
2023-12-23 18:03:51 -0300

    Adicionando novos arquivos

---
+++ inteiro.py
@@ -0,0 +1,7 @@
+# Para tranbalhar com numeros inteiros no python voce informar
+# o tipo do valor que a variável irá receber.
+x = int(input("Qual o valor de x? "))
+y = int(input("Qual o valor de y? "))
+
+# Escreva na tela para o usuário o valor da soma x + y
+print(x + y)
