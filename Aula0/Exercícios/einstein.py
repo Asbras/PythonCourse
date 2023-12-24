eb95ab3eebc1c4354ef2d541c757cb0e5fc5f10f
Asbras <silentbob320@gmail.com>
2023-12-24 16:40:03 -0300

    Adicionando novos arquivos

---
+++ Aula0/Exercícios/einstein.py
@@ -0,0 +1,20 @@
+"""
+    In a file called einstein.py, implement a program in Python that prompts
+    the user for mass as an integer (in kilograms) and then outputs the
+    equivalent number of Joules as an integer. Assume that the user will input
+    an integer.
+"""
+
+
+def converter(massa):
+    c = 300000000
+    energia = massa * c ** 2
+    print("E: ", energia)
+
+
+def main():
+    m = float(input("m: "))
+    converter(m)
+
+
+main()
